af5fa7371404c27c6a14b48051c8aac0e3e11395
Wang Chen <wangvsa@163.com>
2013-12-13 17:29:30 +0800

    完成第九章框架，新建两篇

--- create.py
+++ create.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python
 
 headlist = [
-'Self Encapsulate Field',
-'Replace Data Value with Object',
-'Change Value to Reference',
-'Change Reference to Value',
-'Replace Array with Object',
-'Duplicate Observed Data',
-'Change Unidirectional Association to Bidirectional',
-'Encapsulate Collection',
-'Replace Record with Data Class',
-'Replace Type Code with Class',
-'Replace Type Code with Subclasses',
-'Replace Type Code with State/Strategy',
-'Replace Subclass with Fields'
+'Decompose Conditional',
+'Consolidate Conditional Expression',
+'Consolidate Duplicate Conditional Fragments',
+'Remove Control Flag',
+'Replace Nested Conditional with Guard Clauses',
+'Replace Conditional with Polymorphism',
+'Introduce Null Object',
+'Introduce Assertion'
 ]
+
+basedir = 'simplifying-conditional-expressions'
+
 for element in headlist:
     print(element)
-    filename = element.replace('/',' or ').replace(' ', '-') + '.html'
+    filename = basedir + '/' + element.replace(' ', '-') + '.html'
     f = open(filename, 'w')
     f.write("---\n")
-    f.write('layout: organizing-data\n')
+    f.write('layout: simplifying-conditional-expressions\n')
     f.write('head_en: '+element)
     f.write("\n---")
     f.close()
