d494128f0f3cb18de1f941163ee4be290777b820
Wang Chen <wangvsa@163.com>
2013-12-14 09:55:32 +0800

    新增第十章框架

--- create.py
+++ create.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python
 
 headlist = [
-'Decompose Conditional',
-'Consolidate Conditional Expression',
-'Consolidate Duplicate Conditional Fragments',
-'Remove Control Flag',
-'Replace Nested Conditional with Guard Clauses',
-'Replace Conditional with Polymorphism',
-'Introduce Null Object',
-'Introduce Assertion'
+'Rename Method',
+'Add Parameter',
+'Remove Parameter',
+'Separate Query from Modifier',
+'Parameterize Method',
+'Replace Parameter with Explicit Methods',
+'Preserve Whole Object',
+'Replace Parameter with Method',
+'Introduce Parameter Object',
+'Remove Setting Method',
+'Hide Method',
+'Replace Constructor with Factory Method',
+'Encapsulate Downcast',
+'Replace Error Code with Exception',
+'Replace Exception with Test',
 ]
 
-basedir = 'simplifying-conditional-expressions'
+basedir = 'making-method-calls-simpler'
 
 for element in headlist:
     print(element)
     filename = basedir + '/' + element.replace(' ', '-') + '.html'
     f = open(filename, 'w')
     f.write("---\n")
-    f.write('layout: simplifying-conditional-expressions\n')
+    f.write('layout: making-method-calls-simpler\n')
     f.write('head_en: '+element)
     f.write("\n---")
     f.close()
