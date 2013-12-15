8027db7217da61bbf2bff89be76155943d0c1932
Wang Chen <wangvsa@163.com>
2013-12-15 21:40:27 +0800

    完成第十一章框架

--- create.py
+++ create.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python
 
 headlist = [
-'Rename Method',
-'Add Parameter',
-'Remove Parameter',
-'Separate Query from Modifier',
-'Parameterize Method',
-'Replace Parameter with Explicit Methods',
-'Preserve Whole Object',
-'Replace Parameter with Method',
-'Introduce Parameter Object',
-'Remove Setting Method',
-'Hide Method',
-'Replace Constructor with Factory Method',
-'Encapsulate Downcast',
-'Replace Error Code with Exception',
-'Replace Exception with Test',
+'Pull Up Field',
+'Pull Up Method',
+'Pull Up Constructor Body',
+'Push Down Method',
+'Push Down Field',
+'Extract Subclass',
+'Extract Superclass',
+'Extract Interface',
+'Collapse Hierarchy',
+'Form Template Method',
+'Replace Inheritance with Delegation',
+'Replace Delegation with Inheritance'
 ]
 
-basedir = 'making-method-calls-simpler'
+basedir = 'dealing-with-generalization'
 
 for element in headlist:
     print(element)
     filename = basedir + '/' + element.replace(' ', '-') + '.html'
     f = open(filename, 'w')
     f.write("---\n")
-    f.write('layout: making-method-calls-simpler\n')
-    f.write('head_en: '+element)
+    f.write('layout: ' + basedir + '\n')
+    f.write('head_en: ' + element)
     f.write("\n---")
     f.close()
